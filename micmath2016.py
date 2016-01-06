eeaf107d68a6d6adf926301baf8afcae0ab0b58a
Victor Adam <victor.adam@engie.com>
2016-01-06 18:27:33 +0100

    Optimisation

--- micmath2016.py
+++ micmath2016.py
@@ -1,51 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 
-# Micmath2016
+import fileinput
+from operator import mul
+from unicodedata import normalize
 
-# Permet de trouver les mots dont le produit des lettres est égal à 2016
-# et aux autres années passées.
+YEARS = [''] * 2017
 
-# Auteur : Nathanaël Jourdane
-# Email: nathanael@jourdane.net
-# Date : 1er janvier 2016
-# License: Creative Commons CC-BY (Attribution)
+for word in fileinput.input('mots'):
+    digits = [ord(l) & 31 for l in normalize('NFKD', word.decode('UTF-8')) if l.isalpha()]
+    prod = reduce(mul, digits)
+    if prod <= 2016:
+        YEARS[prod] = YEARS[prod] or '\n*** %d ***\n' % prod
+        YEARS[prod] += '%s : %s = %d\n' % (word.strip(), ' * '.join(map(str, digits)), prod)
 
-# La vidéo de de Micmath proposant le défi :
-# https://www.youtube.com/watch?v=lrcXq3Bd474
-
-# La liste des mots de la langue française :
-# http://www.pallier.org/ressources/dicofr/dicofr.html
-
-# Pour 2016, il y a donc 6 mots:
-# - chaud : 3 * 8 * 1 * 21 * 4 = 2016
-# - flagada : 6 * 12 * 1 * 7 * 1 * 4 * 1 = 2016
-# - pain : 16 * 1 * 9 * 14 = 2016
-# - panai : 16 * 1 * 14 * 1 * 9 = 2016
-# - pian : 16 * 9 * 1 * 14 = 2016
-# - pin : 16 * 9 * 14 = 2016
-
-import unicodedata, string
-
-years = {}
-with open('./mots', 'r') as infile:
-	for word in infile:
-		prod=1
-		formated_word = ''.join(x for x in unicodedata.normalize('NFKD', word) if x in string.ascii_letters).lower()
-		detail=''
-		for l in formated_word:
-			prod *= ord(l)-96
-			detail += ' * ' + str(ord(l)-96)
-
-		for year in reversed(range(2016+1)):
-			if (prod == year):
-				details = word.strip() + ' : ' + detail[3:] + ' = ' + str(year)
-				if year in years:
-					years[year].append(details)
-				else:
-					years[year] = [details]
-
-with open('./years', 'w') as outfile:
-	for year, words in reversed(sorted(years.items())):
-		outfile.write('\n*** ' + str(year) + ' ***\n')
-		for word in words:
-			outfile.write(word + '\n')
+print ''.join(YEARS[::-1])[:-1]
